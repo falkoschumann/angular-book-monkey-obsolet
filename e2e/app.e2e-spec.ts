1b3c6a64832f6deebd42bcbc310611767b25a60f
Falko Schumann <falko.schumann@muspellheim.de>
2018-05-30 19:48:25 +0200

    Ende-zu-Ende-Test beispielhaft implementiert

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('book-monkey App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to bm!');
+    expect(page.getParagraphText()).toEqual('Home');
   });
 });
